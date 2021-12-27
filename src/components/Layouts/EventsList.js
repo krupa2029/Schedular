335c64a6ecd00a9203954ea7dffa9148dc94065a
krupa2029 <kjkotia10@gmail.com>
2021-12-27 16:30:33 +0530

    Reusable Events-List Component It's responsible to render a dynamic list of events Accepts props from EventContentLayout

---
+++ src/components/Layouts/EventsList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Reusable List Component
+
+const EventsList=(props)=> {
+    return (
+        <div>
+            <h2>{props.eventType} Events List</h2>
+        </div>
+    )
+}
+
+export default EventsList;
