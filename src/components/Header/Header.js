2902eaef293dc8d5efc88ffbdd45ac4c5fc301ab
krupa2029 <kjkotia10@gmail.com>
2021-12-27 18:51:50 +0530

    Style: Responsive Header styling using css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "../Styles/Header.module.css";
 
 const Header = (props) => {
-  const pastEventHandler=()=>{
+  const [humburgerIsActive, setHumburgerIsActive] = useState(false);
+  const humburgerHandler = () => {
+    if (humburgerIsActive) {
+      setHumburgerIsActive(false);
+    } else {
+      setHumburgerIsActive(true);
+    }
+  };
+  const pastEventHandler = () => {
+    setHumburgerIsActive(false);
     props.eventType("past");
-  }
-  const upcommingEventHandler=()=>{
+  };
+  const upcommingEventHandler = () => {
+    setHumburgerIsActive(false);
     props.eventType("upcoming");
-  }
+  };
   return (
-    <header>
-      <div>
-        <h1>WordCamp</h1>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <button onClick={pastEventHandler}>Past Event</button>
+    <header className={classes.header}>
+      <nav className={classes.navbar}>
+        <div className={classes.nav_logo}>
+          <h1>WordCamp</h1>
+        </div>
+
+        <ul className={`${classes.nav_menu} ${humburgerIsActive ? `${classes.active}` : ""}`}>
+          <li className={classes.nav_item}>
+            <a className={classes.nav_link} onClick={pastEventHandler}>
+              Past Event
+            </a>
           </li>
-          <li>
-            <button onClick={upcommingEventHandler}>Upcoming Event</button>
+          <li className={classes.nav_item}>
+            <a className={classes.nav_link} onClick={upcommingEventHandler}>
+              Upcoming Event
+            </a>
           </li>
         </ul>
+        <div
+          className={`${classes.hamburger} ${humburgerIsActive ? `${classes.active}` : ""}`}
+          onClick={humburgerHandler}
+        >
+          <span className={classes.bar}></span>
+          <span className={classes.bar}></span>
+          <span className={classes.bar}></span>
+        </div>
       </nav>
     </header>
   );
