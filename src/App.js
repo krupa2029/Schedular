c1fa343f65029ec8bb8442396b3d7ee018affd8d
krupa2029 <kjkotia10@gmail.com>
2021-12-27 16:40:59 +0530

    Manage States and render content conditionally

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import UpcomingEvents from "./components/Events/UpcomingEvents";
+import PastEvents from "./components/Events/PastEvents";
+
 
 const App = () => {
+  const [currentEventType, setCurrentEventType] = useState("upcoming");
+  let content = <UpcomingEvents/>
+
+  const eventTypeHandler = (currentEvent) => {
+    setCurrentEventType(currentEvent);
+  };
+
+  
+  if(currentEventType === 'upcoming'){
+    content = <UpcomingEvents/>
+  }
+  if(currentEventType === 'past'){
+    content = <PastEvents/>
+  }
+
   return (
     <div>
-      <h1>Hello..</h1>
+      <Header eventType={eventTypeHandler}/>
+      <main>{content}</main>
     </div>
   );
 };
-
 
 export default App;
