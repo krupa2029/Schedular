a3a42b0381325da80b34982cb9571377281d513d
krupa2029 <kjkotia10@gmail.com>
2021-12-27 23:25:34 +0530

    Style: List container, List Items

--- src/components/Layouts/EventsList.js
+++ src/components/Layouts/EventsList.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import classes from '../Styles/EventsList.module.css';
 
 // Reusable List Component
 
@@ -6,6 +7,18 @@ const EventsList=(props)=> {
     return (
         <div>
             <h2>{props.eventType} List</h2>
+            <div className={classes.list_Container}>
+            <div className={classes.event_Item}>
+                <h3>Event-Title</h3>
+                <p>Vanue</p>
+                <p>Start-End Date</p>
+            </div>
+            <div className={classes.event_Item}>
+                <h3>Event-Title</h3>
+                <p>Vanue</p>
+                <p>Start-End Date</p>
+            </div>
+            </div>
         </div>
     )
 }
