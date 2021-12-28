48a18d32224e77ffca5b42c734f1dd1243833f68
krupa2029 <kjkotia10@gmail.com>
2021-12-28 19:58:35 +0530

    Sending Http request -Forward loadedData to child components -Sending http request when component load for the first time

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import UpcomingEvents from "./components/Events/UpcomingEvents";
 import PastEvents from "./components/Events/PastEvents";
-
+import { getEventsData } from "./lib/api";
+import useHttp from "./hooks/use-http";
 
 const App = () => {
   const [currentEventType, setCurrentEventType] = useState("upcoming");
-  let content = <UpcomingEvents/>
+
+  const {
+    sendRequest,
+    status,
+    data: loadedData,
+    error,
+  } = useHttp(getEventsData, true);
+
+  useEffect(() => {
+    let isApiSubscribed = true;
+    if (isApiSubscribed) {
+      sendRequest();
+    }
+    return () => {
+      // cancel the subscription
+      isApiSubscribed = false;
+    };
+  }, [sendRequest]);
+
+  let content;
+
+  if (error) {
+    return (content = <p>{error}</p>);
+  }
+  if (status === "completed" && loadedData && loadedData !== null) {
+    console.log(loadedData);
+
+    if (currentEventType === "upcoming") {
+      content = <UpcomingEvents loadedData={loadedData} />;
+    }
+    if (currentEventType === "past") {
+      content = <PastEvents loadedData={loadedData} />;
+    }
+  }
 
   const eventTypeHandler = (currentEvent) => {
     setCurrentEventType(currentEvent);
   };
 
-  
-  if(currentEventType === 'upcoming'){
-    content = <UpcomingEvents/>
-  }
-  if(currentEventType === 'past'){
-    content = <PastEvents/>
-  }
-
   return (
     <div>
-      <Header eventType={eventTypeHandler}/>
+      <Header eventType={eventTypeHandler} />
       <main>{content}</main>
     </div>
   );
