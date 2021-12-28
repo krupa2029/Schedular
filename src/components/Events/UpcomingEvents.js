0a70263e669c169ca37b1c68c8b3eb38e15bdefd
krupa2029 <kjkotia10@gmail.com>
2021-12-28 20:41:01 +0530

    Filter: UpcomingEvents

--- src/components/Events/UpcomingEvents.js
+++ src/components/Events/UpcomingEvents.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { EventContentLayout } from "../Layouts/EventContentLayout";
 
-//Component will be rendered when "upcomingevents" Button got clicked... 
+//Component will be rendered when "upcomingevents" Button got clicked...
 
 const UpcomingEvents = (props) => {
   const eventType = "Upcoming Events";
-  console.log(props.loadedData);
+  // console.log(props.loadedData);
+
+  const allEvents = props.loadedData;
+  const upComingEvents = allEvents.filter(
+    (eventData) => Date.now() < eventData.start
+  );
   return (
     <div>
-      {/* <p>UpcomingEvents</p> */}
-      <EventContentLayout loadedData={props.loadedData} eventType={eventType}/>
+      <EventContentLayout loadedData={upComingEvents} eventType={eventType} />
     </div>
   );
 };
