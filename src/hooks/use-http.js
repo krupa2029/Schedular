afbb528ad2d200fb44fbfe190f8177d944cbe710
krupa2029 <kjkotia10@gmail.com>
2021-12-28 19:26:34 +0530

    Create useHttp custom-hook - send http request Reusable custom react-hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,67 @@
+import { useReducer, useCallback } from "react";
+import 'regenerator-runtime/runtime';
+
+function httpReducer(state, action) {
+  // Sending Request to Server for Data...
+  if (action.type === "SEND") {
+    return {
+      data: null,
+      error: null,
+      status: "pending",
+    };
+  }
+
+  // If get the data successfully...
+  if (action.type === "SUCCESS") {
+    return {
+      data: action.responseData,
+      error: null,
+      status: "completed",
+    };
+  }
+
+  //If don't get expected response...
+  if (action.type === "ERROR") {
+    return {
+      data: null,
+      error: action.errorMessage,
+      status: "completed",
+    };
+  }
+
+  return state;
+}
+
+// When sending Request set startWithPending=true...
+
+function useHttp(requestFunction, startWithPending = false) {
+  const [httpState, dispatch] = useReducer(httpReducer, {
+    status: startWithPending ? "pending" : null,
+    data: null,
+    error: null,
+  });
+
+  // sendRequest will be created/stored when 'requestFunction' have any Changes...
+  const sendRequest = useCallback(
+    async function (requestData) {
+      dispatch({ type: "SEND" });
+      try {
+        const responseData = await requestFunction(requestData);
+        dispatch({ type: "SUCCESS", responseData });
+      } catch (error) {
+        dispatch({
+          type: "ERROR",
+          errorMessage: error.message || "Something went wrong!",
+        });
+      }
+    },
+    [requestFunction]
+  );
+
+  return {
+    sendRequest,
+    ...httpState,
+  };
+}
+
+export default useHttp;
