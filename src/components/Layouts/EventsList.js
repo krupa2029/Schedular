9e478fc171079aa8b8d35d4c2565772dbd535f16
krupa2029 <kjkotia10@gmail.com>
2021-12-28 20:18:49 +0530

    Replaced Dummy Data with fetched event-data

--- src/components/Layouts/EventsList.js
+++ src/components/Layouts/EventsList.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import classes from '../Styles/EventsList.module.css';
+import React from "react";
+import classes from "../Styles/EventsList.module.css";
 
 // Reusable List Component
 
-const EventsList=(props)=> {
-    return (
-        <div>
-            <h2>{props.eventType} List</h2>
-            <div className={classes.list_Container}>
-            <div className={classes.event_Item}>
-                <h3>Event-Title</h3>
-                <p>Vanue</p>
-                <p>Start-End Date</p>
-            </div>
-            <div className={classes.event_Item}>
-                <h3>Event-Title</h3>
-                <p>Vanue</p>
-                <p>Start-End Date</p>
-            </div>
-            </div>
-        </div>
-    )
-}
+const EventsList = (props) => {
+    console.log(props.loadedData);
+  return (
+    <div>
+      <h2>{props.eventType} List</h2>
+      <div className={classes.list_Container}>
+        {props.loadedData.map((eventData) => (
+          <div className={classes.event_Item} key={eventData.key}>
+            <h3>{eventData.title}</h3>
+            <p>{eventData.venue}</p>
+          <p>{`${eventData.start.toDateString()} - ${eventData.end.toDateString()}`}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default EventsList;
