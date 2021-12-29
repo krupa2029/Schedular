c74f392a253e86b26dfcefb65ae9e2565156a696
krupa2029 <kjkotia10@gmail.com>
2021-12-29 17:08:41 +0530

    Manage Content-View Type controls

--- src/components/Layouts/EventContentLayout.js
+++ src/components/Layouts/EventContentLayout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import EventsList from "./EventsList";
 import classes from "../Styles/EventContentLayout.module.css";
 import MapView from "../Map/MapView";
@@ -6,7 +6,18 @@ import MapView from "../Map/MapView";
 // Dynamic Layout for Event-Content
 
 export const EventContentLayout = (props) => {
-  console.log(props.loadedData);
+  const [eventView, setEventView] = useState("Map");
+  // let eventView = <MapView />;
+
+  const mapViewHandler = () => {
+    setEventView("Map");
+  };
+
+  const calenderViewHandler = () => {
+    setEventView("calender");
+  };
+  console.log(eventView);
+  // console.log(props.loadedData);
 
   return (
     <div className={classes.container}>
@@ -16,16 +27,19 @@ export const EventContentLayout = (props) => {
       <div className={classes.content_container}>
         <div className={classes.viewcontainer}>
           <div className={classes.view_control}>
-            <button>Calender</button>
-            <button>MapView</button>
+          <button onClick={calenderViewHandler}>Calender</button>
+            <button onClick={mapViewHandler}>MapView</button>
           </div>
           <div className={classes.view_content}>
-           <MapView/>
+            {eventView === "Map" ? <MapView /> : `Calender`}
           </div>
         </div>
 
         <div className={classes.event_list}>
-          <EventsList loadedData={props.loadedData} eventType={props.eventType} />
+          <EventsList
+            loadedData={props.loadedData}
+            eventType={props.eventType}
+          />
         </div>
       </div>
     </div>
