26dabc96dae8f6258e40e05fb8761dc9bb9975bb
krupa2029 <kjkotia10@gmail.com>
2021-12-30 18:38:02 +0530

    Feat: 'No Events Found' If there are no past or upcomming events found

--- src/components/Layouts/EventsList.js
+++ src/components/Layouts/EventsList.js
@@ -4,19 +4,23 @@ import classes from "../Styles/EventsList.module.css";
 // Reusable List Component
 
 const EventsList = (props) => {
-    console.log(props.loadedData);
+  // console.log(props.loadedData);
+  let listItem = <h2>No Events Found!!</h2>;
+
+  if (props.loadedData !== null) {
+    listItem = props.loadedData.map((eventData) => (
+      <div className={classes.event_Item} key={eventData.key}>
+        <h3>{eventData.title}</h3>
+        <p>{eventData.venue}</p>
+        <p>{`${eventData.start.toDateString()} - ${eventData.end.toDateString()}`}</p>
+      </div>
+    ));
+  }
+
   return (
     <div>
       {/* <h2>{props.eventType} List</h2> */}
-      <div className={classes.list_Container}>
-        {props.loadedData.map((eventData) => (
-          <div className={classes.event_Item} key={eventData.key}>
-            <h3>{eventData.title}</h3>
-            <p>{eventData.venue}</p>
-          <p>{`${eventData.start.toDateString()} - ${eventData.end.toDateString()}`}</p>
-          </div>
-        ))}
-      </div>
+      <div className={classes.list_Container}>{listItem}</div>
     </div>
   );
 };
