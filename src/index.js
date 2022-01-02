68e0249663104559c9cee02ed8d1c30210f4bcc8
krupa2029 <kjkotia10@gmail.com>
2022-01-02 17:49:55 +0530

    Add: "regenerator-runtime/runtime" to fix error

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(<App />, document.querySelector('#root'));
